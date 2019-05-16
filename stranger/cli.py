c28791fc8fbbf939ef197466741ea9d3a882c890
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2019-05-16 09:48:24 +0200

    Fix #7 - Fix #8

--- stranger/cli.py
+++ stranger/cli.py
@@ -6,7 +6,7 @@ import gzip
 from pprint import pprint as pp
 from codecs import (open, getreader)
 
-from stranger.resources import repeats_path
+from stranger.resources import repeats_json_path
 from stranger.utils import (parse_repeat_file, get_repeat_info, get_info_dict, get_variant_line)
 from stranger.vcf_utils import print_headers
 from stranger.__version__ import __version__
@@ -25,7 +25,7 @@ def print_version(ctx, param, value):
 @click.option('-f', '--repeats-file',
     type = click.Path(exists=True),
     help="Path to a file with repeat definitions. See README for explanation",
-    default=repeats_path,
+    default=repeats_json_path,
     show_default=True,
 )
 @click.option('--version', is_flag=True, callback=print_version,
@@ -37,32 +37,32 @@ def cli(context, vcf, repeats_file, loglevel):
     """Annotate str variants with str status"""
     coloredlogs.install(level=loglevel)
     LOG.info("Running stranger version %s", __version__)
-    
+
     repeat_information = None
     repeats_file_type = 'tsv'
     if repeats_file.endswith('.json'):
         repeats_file_type = 'json'
     LOG.info("Parsing repeats file %s", repeats_file)
-    
+
     with open(repeats_file, 'r') as file_handle:
         repeat_information = parse_repeat_file(file_handle, repeats_file_type)
 
     if not repeat_information:
         LOG.warning("Could not find any repeat info")
         context.abort()
-    
+
     stranger_info = 'STR_STATUS'
     stranger_description = "Repeat expansion status. Alternatives in [normal, pre_mutation, full_mutation]"
     stranger_header = '##INFO=<ID={0},Number={1},Type={2},Description="{3}">'.format(
         stranger_info, 'A', 'String', stranger_description
     )
-    
+
     if vcf.endswith('.gz'):
         LOG.info("Vcf is zipped")
         vcf_handle = getreader('utf-8')(gzip.open(vcf), errors='replace')
     else:
         vcf_handle = open(vcf, mode='r', encoding='utf-8', errors='replace')
-    
+
     LOG.info("Parsing variants from %s", vcf)
     for line in vcf_handle:
         line = line.rstrip()
@@ -81,5 +81,5 @@ def cli(context, vcf, repeats_file, loglevel):
         variant_info['info_dict'] = get_info_dict(variant_info['INFO'])
         repeat_string = get_repeat_info(variant_info, repeat_information)
         variant_info['info_dict'][stranger_info] = repeat_string
-        
-        click.echo(get_variant_line(variant_info, header_info)) 
+
+        click.echo(get_variant_line(variant_info, header_info))
