51dbe16872bce09df8c48f086ce8a30d41fd9d0d
fellen31 <felixlenner@gmail.com>
2024-05-17 09:45:58 +0200

    address review comments

--- stranger/utils.py
+++ stranger/utils.py
@@ -158,6 +158,19 @@ def get_exhu_repeat_res_from_alts(variant_info: dict):
             raise SyntaxError("Allele on wrong format")
     return repeat_res
 
+def get_repeat_id(variant_info):
+    """
+    First tries to get variant id from REPID,
+    if that is not sucessful, try to get variant id from TRID (TRGT).
+    If the ID is formatted with underscore (STRchive),
+    grab the part which is after the underscore, otherwise take the whole ID (PacBio).
+    """
+    repeat_id = variant_info['info_dict'].get('REPID')
+    if not repeat_id:
+        trid_value = variant_info['info_dict'].get('TRID')
+        if trid_value:
+            repeat_id = trid_value.split('_', 1)[1] if '_' in trid_value else trid_value
+            return repeat_id
 
 def get_repeat_info(variant_info, repeat_info):
     """Find the correct mutation level of a str variant
@@ -171,17 +184,8 @@ def get_repeat_info(variant_info, repeat_info):
     """
 
     # There can be one or more alternatives (each ind can have at most two of those)
-
-    repeat_id = variant_info['info_dict'].get('REPID')
+    repeat_id = get_repeat_id(variant_info)
     
-    if not repeat_id:
-        trid_value = variant_info['info_dict'].get('TRID')
-        
-        if '_' in trid_value:
-            repeat_id = trid_value.split('_')[1]
-        else:
-            repeat_id = trid_value
-
     if not repeat_id in repeat_info:
         LOG.warning("No info for repeat id %s", repeat_id)
         return None
@@ -219,7 +223,6 @@ def get_repeat_info(variant_info, repeat_info):
 
     return repeat_data
 
-
 def get_trgt_repeat_res(variant_info, repeat_info):
     """Convert target variant info into ExHu count format, splitting entries if needed,
     if they turn out to contain more than one allele or more than one motif.
@@ -228,15 +231,8 @@ def get_trgt_repeat_res(variant_info, repeat_info):
     If no such PATHOLOGIC_STRUC info is available, default to use all motif parts.
     """
 
-    repeat_id = variant_info['info_dict'].get('REPID')
-    if not repeat_id:
-        trid_value = variant_info['info_dict'].get('TRID')
-        
-        if '_' in trid_value:
-            repeat_id = trid_value.split('_')[1]
-        else:
-            repeat_id = trid_value
-
+    repeat_id = get_repeat_id(variant_info)
+    
     if not repeat_id in repeat_info:
         LOG.warning("No info for repeat id %s", repeat_id)
         return None
@@ -252,7 +248,7 @@ def get_trgt_repeat_res(variant_info, repeat_info):
                 
                 # What should we do if MC is . ? 
                 if allele == ".":
-                    continue
+                    repeat_res.extend([0])
 
                 if len(mcs) > 1:
                     pathologic_mcs = repeat_info[repeat_id].get('pathologic_struc', range(len(mcs)))
