53e16eee464dc93776c5631c994de9c953d74357
Lint Action <lint-action@samuelmeuli.com>
2025-06-19 10:15:43 +0000

    Fix code style issues with Black

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -75,13 +75,15 @@ def test_get_trgt_repeat_res(repeats_json_handle):
     assert repeats_info["HTT"]["pathologic_struc"] == [0]
 
     variant_info = {
-        'info_dict': {'TRID': 'HTT', 'END': '3074966', 'MOTIFS': 'CAG,CCG', 'STRUC': '(CAG)nCAACAG(CCG)n',
-                      'FOUND_IN': 'TRGT'},
-        "format_dicts": [{
-        "MC": "27_6, 32_9"
-    }]
+        "info_dict": {
+            "TRID": "HTT",
+            "END": "3074966",
+            "MOTIFS": "CAG,CCG",
+            "STRUC": "(CAG)nCAACAG(CCG)n",
+            "FOUND_IN": "TRGT",
+        },
+        "format_dicts": [{"MC": "27_6, 32_9"}],
     }
 
     repeat_res = get_trgt_repeat_res(variant_info, repeats_info)
-    assert repeat_res == [27,32]
-
+    assert repeat_res == [27, 32]
